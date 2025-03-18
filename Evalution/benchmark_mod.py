235222b9fd20c2ca9c6bc4eb69220e899e3a11b4
Trailblazer <54266311+Shi-Jinhui@users.noreply.github.com>
2025-03-18 21:37:34 +0800

    benchmark_mod.py

--- Evalution/benchmark_mod.py
+++ Evalution/benchmark_mod.py
@@ -3,8 +3,8 @@ import os
 from os.path import join as pj
 import argparse
 import sys
-sys.path.append("modules")
-import utils
+sys.path.append(os.path.abspath('./MINERVA'))
+from modules import utils
 import numpy as np
 import torch as th
 import scib
@@ -18,8 +18,8 @@ from scipy.stats import pearsonr
 
 # %%
 parser = argparse.ArgumentParser()
-parser.add_argument('--task', type=str, default='bmmc_sub100')
-parser.add_argument('--experiment', type=str, default='null')
+parser.add_argument('--task', type=str, default='dm_sub10')
+parser.add_argument('--experiment', type=str, default='e0')
 parser.add_argument('--model', type=str, default='default')
 parser.add_argument('--method', type=str, default='embed')
 o, _ = parser.parse_known_args()  # for python interactive
@@ -29,18 +29,18 @@ end = 1000
 step = 100
 
 # %%
-data_dir = pj("/dev/shm/processed/", o.task)
-data_config = utils.load_toml("configs/data.toml")[o.task]
+data_dir = pj("./result/preprocess/", o.task)
+data_config = utils.load_toml("./MINERVA/configs/data.toml")[o.task]
 for k, v in data_config.items():
     vars(o)[k] = v
-model_config = utils.load_toml("configs/model.toml")["default"]
+model_config = utils.load_toml("./MINERVA/configs/model.toml")["default"]
 if o.model != "default":
-    model_config.update(utils.load_toml("configs/model.toml")[o.model])
+    model_config.update(utils.load_toml("./MINERVA/configs/model.toml")[o.model])
 for k, v in model_config.items():
     vars(o)[k] = v
 o.s_joint, o.combs, *_ = utils.gen_all_batch_ids(o.s_joint, o.combs)
 
-# %%
+
 def mod_evaluate(o, pred, data_dir, result_dir):
     output_type = "embed"
     embed = "X_emb"
@@ -49,6 +49,7 @@ def mod_evaluate(o, pred, data_dir, result_dir):
     mod_key = "modality"
     cluster_key = "cluster"
     si_metric = "euclidean"
+    subsample = 0.5
     verbose = False
 
     results1 = {
@@ -64,54 +65,79 @@ def mod_evaluate(o, pred, data_dir, result_dir):
         "RMSLE_adt": {},
     }
 
-    results3 = {
-        "pearson_rna": {},
-        "pearson_adt": {},
-    }
-
-    results4= {
-        "RMSLE_rna": {},
-        "RMSLE_adt": {},
-    }
-
     knn = KNeighborsClassifier(n_neighbors=5, weights='distance')
 
     for batch_id in pred.keys():
-        
+
         print("Processing batch: ", batch_id)
+        z = pred[batch_id]["z"]
         x = pred[batch_id]["x"]
-        x_impt = pred[batch_id]["x_impt"]
+        x_trans = pred[batch_id]["x_trans"]
+        mask_dir = pj(data_dir, "subset_"+str(batch_id), "mask")
 
-        mask_dir = pj("/dev/shm/processed", o.task, "subset_"+str(batch_id), "mask")
+        c = {m: v[:, :o.dim_c] for m, v in z.items()}
+        c_cat = np.concatenate((c["rna"], c["adt"]), axis=0)
+        mods_cat = ["rna"]*len(c["rna"]) + ["adt"]*len(c["adt"])
+
+        label = utils.load_csv(pj("./result/preprocess/", o.task, "subset_" + str(batch_id), "labels.csv"))[1:]
+        label = np.array(utils.transpose_list(label)[6])
+        label_cat = np.tile(label, 2)
+
+        assert len(c_cat) == len(mods_cat) == len(label_cat), "Inconsistent lengths!"
+
         batch = str(batch_id) # toml dict key must be str
+        # print("Computing asw_mod")
+        adata = ad.AnnData(c_cat)
+        adata.obsm[embed] = c_cat
+        adata.obs[mod_key] = mods_cat
+        adata.obs[mod_key] = adata.obs[mod_key].astype("category")
+        adata.obs[label_key] = label_cat
+        adata.obs[label_key] = adata.obs[label_key].astype("category")
 
-        results1["foscttm"][batch], results1["f1"][batch] = {}, {}
-        results2["pearson_rna"][batch], results2["pearson_adt"][batch], results2["RMSLE_rna"][batch], results2["RMSLE_adt"][batch] = {}, {}, {}, {}
-        results3["pearson_rna"][batch], results3["pearson_adt"][batch] = {}, {}
-        results4["RMSLE_rna"][batch], results4["RMSLE_adt"][batch] = {}, {}
+        results1["asw_mod"][batch] = me.silhouette_batch(adata, batch_key=mod_key,
+            group_key=label_key, embed=embed, metric=si_metric, verbose=verbose)
+
+        results1["foscttm"][batch] = {}
+        results1["f1"][batch] = {}
+
+        results2["pearson_rna"][batch] = {}
+        results2["pearson_adt"][batch] = {}
+        results2["RMSLE_rna"][batch] = {}
+        results2["RMSLE_adt"][batch] = {}
 
         for m in c.keys() - {"joint"}:
             for m_ in set(c.keys()) - {m, "joint"}:
                 k = m+"_to_"+m_
+                # print(k+":")
+                # print("Computing foscttm")
                 results1["foscttm"][batch][k] = 1 - utils.calc_foscttm(th.from_numpy(c[m]), th.from_numpy(c[m_]))
-                
+
                 # print("Computing f1")
                 knn.fit(c[m], label)
                 label_pred = knn.predict(c[m_])
+                # cm = confusion_matrix(label, label_pred, labels=knn.classes_)
                 results1["f1"][batch][k] = f1_score(label, label_pred, average='micro')
-                
+
                 if m_ in ["rna", "adt"]:
                     mask = np.array(utils.load_csv(pj(mask_dir, m_+".csv"))[1][1:]).astype(bool)
                     x_gt = x[m_][:, mask].reshape(-1)
                     x_pred = x_trans[k][:, mask].reshape(-1)
                     # print("Computing pearson_"+m_)
-                    results2["pearson_"+m_][batch][k] = pearsonr(x_gt.reshape(-1), x_pred.reshape(-1))[0]
+                    results2["pearson_"+m_][batch][k] = pearsonr(x_gt, x_pred)[0]
+                    # results2["pearson_"+m_][batch] = [pearsonr(x_gt[i], x_pred[i])[0] for i in np.arange(x_pred.shape[0])]
+                    # results2["RMSLE_"+m][batch] = [np.sqrt(np.mean(np.power(np.log(x_gt[i] + 1) - np.log(x_pred[i] + 1), 2))) for i in np.arange(x_pred.shape[0])]
                     results2["RMSLE_"+m_][batch][k] = np.sqrt(np.mean(np.power(np.log(x_gt + 1) - np.log(x_pred + 1), 2)))
-                    results3["pearson_"+m_][batch] = [pearsonr(x_gt[:,i].reshape(-1), x_pred[:,i].reshape(-1))[0] for i in np.arange(x_pred.shape[1])]
-                    results4["RMSLE_"+m_][batch] = [np.sqrt(np.mean(np.power(np.log(x_gt[:,i] + 1) - np.log(x_pred[:,i] + 1), 2))) for i in np.arange(x_pred.shape[1])]            
-                    
+
 
     results1_avg = {metric: np.mean(utils.extract_values(v)) for metric, v in results1.items()}
+    df1 = pd.DataFrame({
+        'ASW_mod':          [results1_avg['asw_mod']],
+        'FOSCTTM':          [results1_avg['foscttm']],
+        'Label_transfer':   [results1_avg['f1']]
+    })
+    print(df1)
+
+    results2_avg = {metric: np.mean(utils.extract_values(v)) for metric, v in results2.items()}
     df2 = pd.DataFrame({
         'Pearson_RNA':      [results2_avg['pearson_rna']],
         'Pearson_ADT':      [results2_avg['pearson_adt']],
@@ -120,21 +146,8 @@ def mod_evaluate(o, pred, data_dir, result_dir):
     })
     print(df2)
 
-    merged_dict = {k: list(v.values()) for k, v in results3.items()}
-    for key, value in merged_dict.items():
-        csv_file = f'{result_dir}/{key}.csv'
-        with open(csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(value)
+    return df1, df2
 
-    merged_dict = {k: list(v.values()) for k, v in results4.items()}
-    for key, value in merged_dict.items():
-        csv_file = f'{result_dir}/{key}.csv'
-        with open(csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(value)
-
-    return df2
 
 
 # %%
@@ -145,7 +158,7 @@ for i in list(range(start, end, step)):
     result_dir = pj("result", "comparison", o.task, o.experiment)
     # Load predicted latent variables
     o.pred_dir = pj("result", o.task, o.experiment, o.model, "predict", init_model)
-    pred = utils.load_predicted(o, mod_latent=True, translate=True, input=True, group_by="subset")
+    pred = utils.load_predicted(o, mod_latent=True, translate=True, input=True,impute=True, group_by="subset")
     df_mod, df_impu = mod_evaluate(o, pred, data_dir, result_dir)
     df_mod["Model"] = "Step_" + "{:04d}".format(i + 1)
     df_impu["Model"] = "Step_" + "{:04d}".format(i + 1)
